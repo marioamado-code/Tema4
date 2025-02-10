6dc93a5fe98b5e53c325ae0c77332a106499d666
marioamado-code <mario.amado@iesnervion.es>
2025-02-10 12:43:18 +0100

    commit cuenta corriente

--- src/parte2/ejercicio1/CuentaCorriente.java
+++ src/parte2/ejercicio1/CuentaCorriente.java
@@ -1,5 +1,28 @@
 package parte2.ejercicio1;
 
 public class CuentaCorriente {
-
+private String DNI;
+private String nombre;
+private double saldo;
+enum Nacionalidad {ESPAÑOLA,EXTRANJERA};
+private Nacionalidad nacion;
+public CuentaCorriente(String DNI,double saldo) {
+	this.DNI=DNI;
+	this.saldo=saldo;
+	
+}
+public CuentaCorriente(String DNI,double saldo,String nombre) {
+	this.DNI=DNI;
+	this.saldo=saldo;
+	this.nombre=nombre;
+	
+}
+public CuentaCorriente(String DNI,double saldo,String nombre,String nacionalidad) {
+	this.DNI=DNI;
+	this.saldo=saldo;
+	this.nombre=nombre;
+	this.nacion=Nacionalidad.valueOf(nacionalidad);
+	
+	
+}
 }
--- src/parte2/ejercicio1/PrincipalCuentaCorriente.java
+++ src/parte2/ejercicio1/PrincipalCuentaCorriente.java
@@ -3,7 +3,7 @@ package parte2.ejercicio1;
 public class PrincipalCuentaCorriente {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
