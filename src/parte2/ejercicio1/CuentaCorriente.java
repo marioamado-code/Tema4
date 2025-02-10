f0d1db1a87b4d127ebb673bae9f04cb3ee0128a3
marioamado-code <mario.amado@iesnervion.es>
2025-02-10 12:42:58 +0100

    cuenta corriente principio

---
+++ src/parte2/ejercicio1/CuentaCorriente.java
@@ -0,0 +1,5 @@
+package parte2.ejercicio1;
+
+public class CuentaCorriente {
+
+}
---
+++ src/parte2/ejercicio1/PrincipalCuentaCorriente.java
@@ -0,0 +1,10 @@
+package parte2.ejercicio1;
+
+public class PrincipalCuentaCorriente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
